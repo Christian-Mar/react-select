ab2ef6122964e7fb3675c78004fcd1da77de3663
Christian <71664957+Christian-Mar@users.noreply.github.com>
2021-08-06 13:42:43 +0200

    Select with styling

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Select from 'react-select';
 //npm i --save react-select
 
+
+
 function App() {
 
   const options=[
@@ -19,6 +21,27 @@ function App() {
       label: 'label-three',
     }
   ]
+  
+  const customStyles = {
+  option: (provided, state) => ({
+    //...provided,
+    borderBottom: '1px dotted pink',
+    color: state.isSelected ? 'red' : 'pink',
+    padding: 20,
+  }),
+  control: () => ({
+    // none of react-select's styles are passed to <Control />
+    width: 200,
+    color: 'blue',
+  }), 
+  singleValue: (provided, state) => {
+    const opacity = state.isDisabled ? 0.8 : 1;
+    const transition = 'opacity 300ms';
+    const color = 'gold'
+
+    return { ...provided, opacity, transition, color };
+  }
+}
 
   return (
     <div className="App">
@@ -27,10 +50,20 @@ function App() {
         <p>
           This is an example of how to make a dropdown button in React, since it's difficult to style the options in just HTML. 
         </p>
-        <div style={{width: '15rem'}}>
+        <div>
         <Select
         options={options}
-          placeholder="Select"/>
+        styles={customStyles}
+        label="dropdown"
+        placeholder="Select"
+        theme={theme => ({
+    ...theme,
+    borderRadius: 0,
+    colors: {
+      ...theme.colors,
+      neutral50: 'orange',  // Placeholder color
+    }
+  })}/>
         </div>
         <a
           className="App-link"
