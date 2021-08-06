af5b368d9802db10da39b2e1a937c56bfe461240
Christian <71664957+Christian-Mar@users.noreply.github.com>
2021-08-06 10:26:44 +0200

    Commit button without styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import Select from 'react-select';
+//npm i --save react-select
 
 function App() {
+
+  const options=[
+    {
+      value: 'value-one',
+      label: 'label-one',
+    },
+    {
+      value: 'value-two',
+      label: 'label-two',
+    },
+    {
+      value: 'value-three',
+      label: 'label-three',
+    }
+  ]
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is an example of how to make a dropdown button in React, since it's difficult to style the options in just HTML. 
         </p>
+        <div style={{width: '15rem'}}>
+        <Select
+        options={options}
+          placeholder="Select"/>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
